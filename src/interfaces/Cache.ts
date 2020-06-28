9a3eff8a70eaec420ed28191bdad5ffa146b9681
lukesrw <lukesrw+github@gmail.com>
2020-06-28 11:45:02 +0100

    stole interface definitions from compile

--- src/interfaces/Cache.ts
+++ src/interfaces/Cache.ts
@@ -1,3 +1,5 @@
+import { PathLike } from "fs";
+
 import Stat from "../lib/Stat";
 
 export interface CacheDataRecordInterface {
@@ -21,4 +23,19 @@ export interface CacheOptionsInterface {
 export interface CacheInterface {
     data: CacheDataInterface;
     options: CacheOptionsInterface;
+    setOptions(options?: CacheOptionsInterface): this;
+    toJSON(): string;
+    toJSONFile(location: PathLike): Promise<string>;
+    toJSONFileSync(location: PathLike): string;
+    toPack(): Promise<Buffer>;
+    toPackSync(): Buffer;
+    toPackFile(location: PathLike): Promise<Buffer>;
+    toPackFileSync(location: PathLike): Buffer;
+    set<DataType>(key: string | Function, data: DataType, args?: Array<any> | string): DataType;
+    inspect(key: string | Function, args?: Array<any> | string): CacheDataRecordInterface | undefined;
+    get(key: string | Function, args?: Array<any> | string): any;
+    use(func: Function, args?: any[], use_cache?: Function | boolean): Promise<any>;
+    useSync(func: Function, args?: any[], use_cache?: Function | boolean): any;
+    unset(key: string | Function, args: Array<any> | string): this;
+    clean(): this;
 }
--- src/interfaces/Stat.ts
+++ src/interfaces/Stat.ts
@@ -1,5 +1,13 @@
 export interface StatInterface {
     values: number[];
+    add(...items: number[]): this;
+    getLast(fallback?: number): number;
+    getAverage(): number;
+    getCount(): number;
+    getMax(): number;
+    getMin(): number;
+    getSum(): number;
+    toCompact(): StatCompactInterface;
 }
 
 export interface StatCompactInterface {
